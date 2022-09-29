f050e8ca79fb1e843ed4f181daedfa81b7333a37
max <max.lillie95@gmail.com>
2022-09-29 15:18:57 -0500

    remapped equipment to be in arrays of 4 for easier frontend

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,6 +1,15 @@
 class EquipmentController < ApplicationController
   def index
     @equipment = Equipment.all
+    @equipment = @equipment.sort_by(&:equipment_name)
+    equipment_arrays = []
+    length = @equipment.length
+    i = 0
+    while (i < 7)
+      equipment_arrays << @equipment.slice(i * 4, 4)
+      i += 1
+    end
+    @equipment = equipment_arrays
     render template: "equipment/index"
   end
 end
